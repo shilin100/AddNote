e7f21d7826404661458067a2dbae1deb9ba9128f
123 <a123@123demacbook-pro.local>
2018-05-30 17:41:52 +0800

    111

---
+++ AddNote/Sections/Model/NoteModel.swift
@@ -0,0 +1,30 @@
+//
+//  NoteModel.swift
+//  AddNote
+//
+//  Created by 123 on 2018/5/30.
+//  Copyright © 2018年 shilin. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class NoteModel: Object {
+    @objc dynamic var id = UUID().uuidString
+    @objc dynamic var type = ""
+    @objc dynamic var money = ""
+    @objc dynamic var date = ""
+    @objc dynamic var interval = 0.0
+    @objc dynamic var age = 0
+    
+    override class func indexedProperties() -> [String] {
+        return ["date"]
+    }
+
+    override class func primaryKey() -> String? {
+        
+        return "id"
+    }
+    
+    
+}
--- AddNote/Sections/NotesViewController.swift
+++ AddNote/Sections/NotesViewController.swift
@@ -10,8 +10,8 @@ import UIKit
 import ReactorKit
 import RxCocoa
 import RxSwift
-import RxDataSources
-import Differentiator
+import RealmSwift
+import Realm
 
 
 class NotesViewController: UIViewController,StoryboardView {
@@ -20,6 +20,11 @@ class NotesViewController: UIViewController,StoryboardView {
     @IBOutlet weak var addBtn: UIButton!
     @IBOutlet weak var tableview: UITableView!
     
+    var dataArr : Array<Any>?
+    
+    
+    
+
     var disposeBag = DisposeBag()
     typealias Reactor = NotesViewReactor
 
@@ -30,19 +35,32 @@ class NotesViewController: UIViewController,StoryboardView {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-
-        
         tableview.separatorStyle = .none
         tableview.delegate = self
         tableview.dataSource = self
+        let note = NoteModel()
+        note.type = "Rex"
+        note.age = 1
+        note.money = "eee"
+        note.date = "33"
+        note.interval = 1.11
 
+        let realm = try! Realm()
+
+        let dogs = realm.objects(NoteModel.self)
+        if dogs.count > 0 {
+            
+            
+        }
     }
 
+
     
-
-
     func setupUI() {
+
+        
         self.navigationController?.navigationBar.titleTextAttributes = [kCTForegroundColorAttributeName:UIColor.white] as [NSAttributedStringKey : Any]
+
         // 2.设置导航栏前景色：设置item指示色
         self.navigationController?.navigationBar.tintColor = UIColor.purple
         // 3.设置导航栏半透明
@@ -68,7 +86,7 @@ class NotesViewController: UIViewController,StoryboardView {
 
 extension NotesViewController:UITableViewDataSource,UITableViewDelegate{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3;
+        return dataArr?.count ?? 0;
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -77,7 +95,7 @@ extension NotesViewController:UITableViewDataSource,UITableViewDelegate{
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! NoteTableViewCell
-
+        
         cell.left.text = "left"
         cell.right.text = "right"
 
